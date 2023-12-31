593cfec90f6ff65fdd1912d6871fbf6987dbd143
wfzirk <wfzirk@gmail.com>
2023-12-31 15:23:17 -0600

    Change doEnv_ttf.bat
    Create none existent directories

--- doEnv_ttf.bat
+++ doEnv_ttf.bat
@@ -5,7 +5,7 @@ set ver=x.x
 set ttffont=Input/times.ttf
 set language=YOR
 ::set basename=Font_Sun22_07_08-YOR
-set basename=Sun22_07_08_BF-ENG
+set basename=example_YOR
 
 set infile=input/%basename%.csv
 set sfdfile=dist/%basename%
